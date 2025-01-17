cab9ef9e63a92ac1cb9396a83537d2e4a11e3ea7
Alex <alexnguyenly@gmail.com>
2025-01-16 19:07:37 -0500

    Set up

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
 app.listen(3000, () => {
     console.log(`Server started. Listening of port ${port}.`)
 })
+
+console.log(process.env.CLIENT_SECRET);
